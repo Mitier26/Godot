9996183a40211ca6d79e87bff5a789701ab3c5dd
Molkang <mitier26@gmail.com>
2024-07-12 22:29:52 +0900

    히트 박스

--- Space Shooter/Enemies/enemy.gd
+++ Space Shooter/Enemies/enemy.gd
@@ -6,8 +6,14 @@ extends Node2D
 @onready var scale_component = $ScaleComponent
 @onready var flash_component = $FlashComponent
 
+@onready var hurtbox_component = $HurtboxComponent
+@onready var hitbox_component = $HitboxComponent
+
 
 func _ready():
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-	
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+			queue_free()
+	)
+		
 	
