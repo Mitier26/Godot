6a08e5d7998a7958b8fbe8a157a3da4a5f45e2dc
Molkang <mitier26@gmail.com>
2024-07-15 19:34:09 +0900

    저장

--- Space Shooter/menus/game_over.gd
+++ Space Shooter/menus/game_over.gd
@@ -1,11 +1,17 @@
 extends Control
 
+const SAVE_PATH = "user://save.cfg"
+const TEST_SAVE_PATH = "res://save.cfg"
+
 @export var game_stats: GameStats
 
 @onready var score_value = %ScoreValue
 @onready var high_score_value = %HighScoreValue
 
+var save_path = TEST_SAVE_PATH
+
 func _ready():
+	load_highscore()
 	if game_stats.score > game_stats.highscore:
 		game_stats.highscore = game_stats.score
 	score_value.text = str(game_stats.score)
@@ -15,3 +21,14 @@ func _process(delta):
 	if Input.is_action_just_released("ui_accept"):
 		game_stats.score = 0
 		get_tree().change_scene_to_file("res://menus/menu.tscn")
+
+func load_highscore():
+	var config = ConfigFile.new()
+	var error = config.load(save_path)
+	if error != OK : return
+	game_stats.highscore = config.get_value("game", "highscore")
+	
+func save_highscore():
+	var config = ConfigFile.new()
+	config.set_value("game", "highscore", game_stats.highscore)
+	config.save(save_path)
--- Space Shooter/world.gd
+++ Space Shooter/world.gd
@@ -10,6 +10,8 @@ func _ready():
 	update_score_label(game_stats.score)
 	game_stats.score_change.connect(update_score_label)
 	
+	
+	
 	ship.tree_exited.connect(func():
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://menus/game_over.tscn")
