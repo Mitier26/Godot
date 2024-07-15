c3b4b8089243aca2a35db578e1e5d79da008bb4c
Molkang <mitier26@gmail.com>
2024-07-15 14:45:06 +0900

    게임 시작, 게임 오버

---
+++ Space Shooter/world.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var ship = $Ship
+
+func _ready():
+	ship.tree_exited.connect(func():
+		await get_tree().create_timer(1.0).timeout
+		get_tree().change_scene_to_file("res://menus/game_over.tscn")
+	)
