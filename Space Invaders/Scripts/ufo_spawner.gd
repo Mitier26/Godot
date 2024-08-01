5820998d9f21b0483f9bfa94aab9e9c4cf94f823
Molkang <mitier26@gmail.com>
2024-08-02 00:05:28 +0900

    UFO

---
+++ Space Invaders/Scripts/ufo_spawner.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var spawn_timer = $SpawnTimer
+@export var ufo_scene : PackedScene = preload("res://Scenes/ufo.tscn")
+
+func _ready():
+	(spawn_timer as SpawnTimer).setup_timer()
+
+func _on_spawn_timer_timeout():
+	var ufo = ufo_scene.instantiate()
+	ufo.global_position = position
+	get_tree().root.add_child(ufo)
