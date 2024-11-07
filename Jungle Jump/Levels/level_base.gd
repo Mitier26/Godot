edb3676151c9ec00910a5ec27e1791023ed3baaa
Molkang <mitier26@gmail.com>
2024-11-07 21:32:43 +0900

    Jungle Jump

--- Jungle Jump/Levels/level_base.gd
+++ Jungle Jump/Levels/level_base.gd
@@ -3,6 +3,7 @@ extends Node2D
 signal score_changed
 
 var item_scene = load("res://Items/item.tscn")
+var door_scene = load("res://Items/door.tscn")
 
 var score = 0: set = set_score
 
@@ -11,6 +12,7 @@ func _ready():
 	$Player.reset($SpawnPoint.position)
 	set_camera_limits()
 	spawn_items()
+	create_ladder()
 	# 플레이어의 life_change에 연결한다.
 	$Player.life_change.connect($CanvasLayer/HUD.update_life)
 	score_changed.connect($CanvasLayer/HUD.update_score)
@@ -27,10 +29,16 @@ func spawn_items():
 	for cell in item_cells:
 		var data = $Items.get_cell_tile_data(0, cell)
 		var type = data.get_custom_data("type")
-		var item = item_scene.instantiate()
-		add_child(item)
-		item.init(type, $Items.map_to_local(cell))
-		item.picked_up.connect(self._on_item_picked_up)
+		if type == "door":
+			var door = door_scene.instantiate()
+			add_child(door)
+			door.position = $Items.map_to_local(cell)
+			door.boyd_enterd.connect(_on_door_entered)
+		else :
+			var item = item_scene.instantiate()
+			add_child(item)
+			item.init(type, $Items.map_to_local(cell))
+			item.picked_up.connect(self._on_item_picked_up)
 
 func _on_item_picked_up():
 	score += 1
@@ -38,6 +46,28 @@ func _on_item_picked_up():
 func set_score(value):
 	score = value
 	score_changed.emit(score)
+	
+func create_ladder():
+	var cells = $World.get_used_cells(0)
+	for cell in cells:
+		var data = $World.get_cell_tile_data(0, cell)
+		if data.get_custom_data("special") == "ladder":
+			var c = CollisionShape2D.new()
+			$Ladders.add_child(c)
+			c.position = $World.map_to_local(cell)
+			var s = RectangleShape2D.new()
+			s.size = Vector2(8,16)
+			c.shape = s
 
 func _on_player_died():
 	GameState.restart()
+
+func _on_door_entered(body):
+	GameState.next_level()
+
+
+func _on_ladder_body_entered(body):
+	body.is_on_ladder = true
+
+func _on_ladder_body_exited(body):
+	body.is_on_ladder = false
--- Jungle Jump/player/player.gd
+++ Jungle Jump/player/player.gd
@@ -8,8 +8,19 @@ signal died
 @export var run_speed = 150
 @export var jump_speed = -300
 
+# 이중점프
+@export var max_jumps = 2
+@export var double_jump_factor = 1.5
+
+var jump_count = 0
+
+# 사다리
+@export var climb_speed = 50
+
+var is_on_ladder = false
+
 # 상태
-enum {IDLE, RUN, JUMP, HURT, DEAD}
+enum {IDLE, RUN, JUMP, HURT, CLIMB, DEAD}
 var state = IDLE
 
 # 체력
@@ -44,17 +55,27 @@ func change_state(new_state):
 				change_state(DEAD)
 		JUMP:
 			$AnimationPlayer.play("jump_up")
+			jump_count = 1
+		CLIMB:
+			$AnimationPlayer.play("climb")
 		DEAD:
 			hide()
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
+	if state != CLIMB:
+		velocity.y += gravity * delta
 	get_input()
 	
 	move_and_slide()
 	
 	if state == HURT:
 		return
+		
+	# 점프 재설정
+	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+		jump_count = 0
+	
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		if collision.get_collider().is_in_group("danger"):
@@ -73,6 +94,8 @@ func get_input():
 	var right = Input.is_action_pressed("right")
 	var left = Input.is_action_pressed("left")
 	var jump = Input.is_action_just_pressed("jump")
+	var up = Input.is_action_pressed("up")
+	var down = Input.is_action_pressed("down")
 	
 	# 모든 상태에서 일어나는 행동
 	velocity.x = 0
@@ -82,6 +105,13 @@ func get_input():
 	if left : 
 		velocity.x -= run_speed
 		$Sprite2D.flip_h = true
+	
+	# 이중 점프
+	if jump and state == JUMP and jump_count < max_jumps and jump_count > 0:
+		$AnimationPlayer.play("jump_up")
+		velocity.y = jump_speed / double_jump_factor
+		jump_count += 1
+		
 	# 땅에 있을 때만 점프 가능
 	if jump and is_on_floor():
 		change_state(JUMP)
@@ -97,6 +127,23 @@ func get_input():
 		change_state(JUMP)
 	# 착지
 	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+		$Dust.emitting = true
+	
+	# 사다리
+	if up and state != CLIMB and is_on_ladder:
+		change_state(CLIMB)
+	if state == CLIMB:
+		if up:
+			velocity.y = -climb_speed
+			$AnimationPlayer.play("climb")
+		elif down :
+			velocity.y = climb_speed
+			$AnimationPlayer.play("climb")
+		else:
+			velocity.y = 0
+			$AnimationPlayer.stop()
+	if state == CLIMB and not is_on_ladder:
 		change_state(IDLE)
 	
 	# 떨어짐
